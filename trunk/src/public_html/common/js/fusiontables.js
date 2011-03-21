616c0a66a46df8d42d315967f15a1d70f93e34df
tksyokoyama@gmail.com <tksyokoyama@gmail.com@b4b0d59b-2ca1-8d5f-c82c-ad28f7e131bf>
2011-03-21 13:55:11 +0000

    2011.03.21 Hack For Japan

---
+++ trunk/src/public_html/common/js/fusiontables.js
@@ -0,0 +1,9 @@
+
+//
+
+function initFusionTables() {
+  var fusionTablesLayer = new google.maps.FusionTablesLayer(598952);
+  fusionTablesLayer.setMap(map);
+}
+
+initFunctionArray.push(initFusionTables);
